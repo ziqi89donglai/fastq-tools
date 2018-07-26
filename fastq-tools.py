d502477c643387846308083de3fbd8473cdfe7a9
yudong.temp2 <yudong.hu@admerahealth.com.cn>
2018-07-26 14:48:52 +0800

    modify the -m to -mm  in option parser

--- fastq-tools.py
+++ fastq-tools.py
@@ -7,7 +7,7 @@ def parse_args():
 	parse = argparse.ArgumentParser(description='This script is used to transform the fastq/fasta file')
 	parse.add_argument('-i','--input',action='store',required=True,help='input fastq/fasta file')
 	parse.add_argument('-t','--type',action='store',default='fastq',help='input file type:fastq(default) or fasta')
-	parse.add_argument('-m','--method',action='store',required=True,choices=['reverse','reverse_complement','fastq2fasta','fasta2fastq'],help='the transform method')
+	parse.add_argument('-mm','--method',action='store',required=True,choices=['reverse','reverse_complement','fastq2fasta','fasta2fastq'],help='the transform method')
 	parse.add_argument('-v','--verbose',action='store_true',default=False,help='if ouput log messages(default:False)')
 	parse.add_argument('-o','--output',action='store',required=True,help='output file name')
 	args = parse.parse_args()
